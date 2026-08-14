d430516e1c0851ddc83a50bf7632a26a70aea59b
Rustum Jordan <rustumjordan@gmail.com>
2026-08-14 13:22:25 +1200

    created frontend boilerplate and create separate folder for backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center" className='text-center'>
+        Hello World
+      </section>
+
+    </>
+  )
+}
+
+export default App
